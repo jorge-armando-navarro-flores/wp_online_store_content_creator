d97d31defdaa16f2568cf4eb6698f9fc5606e566
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2023-06-24 12:23:12 -0600

    checkpoint

--- ContentGenerator.py
+++ ContentGenerator.py
@@ -21,56 +21,87 @@ class StoreContentGenerator:
         self.store = store
         self.products = products
         self.content = {}
+        self.checkpoint = {
+                            "start_content_structure": 0,
+                            "set_categories": 0,
+                            "set_subcategories": {
+                                "section_idx": 0,
+                                "category_idx": 0,
+                                "idx": 0
+                            },
+                            "set_products": {
+                                "category_idx": 0,
+                                "subcategory_idx": 0,
+                                "idx": 0
+                            }
+                        }
 
     def start_content_structure(self):
-        main_menu_prompt = f"""
-        Instrucciones:
-        Escribe como un experto SEO.
+        print("\n--------INITIAL STRUCTURE SET-------")
+        if not self.checkpoint["start_content_structure"]:
+            main_menu_prompt = f"""
+            Instrucciones:
+            Escribe como un experto SEO.
+    
+            Este es un diccionario que representa un listado de las secciones que debe tener el menu principal de una {self.store}. Tu objetivo es proporcionar la estructura del diccionario en formato JSON. solo el JSON
+    
+            - El diccionario principal se llama "menu".
+            - Cada nombre de sección debe ser una clave dentro del diccionario principal.
+            - Para cada sección, incluye las siguientes claves:
+                - "titulo":  nombre de la seccion.
+                - "descripcion": nombre completo relacionado con {self.store}.
+                - "categorizable" : 1 si es categorizable y 0 si no.
+            """
 
-        Este es un diccionario que representa un listado de las secciones que debe tener el menu principal de una {self.store}. Tu objetivo es proporcionar la estructura del diccionario en formato JSON. solo el JSON
+            main_menu_response = get_completion(main_menu_prompt)
+            main_menu_dict = json.loads(main_menu_response)
 
-        - El diccionario principal se llama "menu".
-        - Cada nombre de sección debe ser una clave dentro del diccionario principal.
-        - Para cada sección, incluye las siguientes claves:
-            - "titulo":  nombre de la seccion.
-            - "descripcion": nombre completo relacionado con {self.store}.
-            - "categorizable" : 1 si es categorizable y 0 si no.
-        """
+            if main_menu_dict['menu'].get('blog'):
+                main_menu_dict['menu']['blog']['categorizable'] = 1
 
-        main_menu_response = get_completion(main_menu_prompt)
-        main_menu_dict = json.loads(main_menu_response)
-
-        if main_menu_dict['menu'].get('blog'):
-            main_menu_dict['menu']['blog']['categorizable'] = 1
-
-        self.content = main_menu_dict
+            self.content = main_menu_dict
+            self.checkpoint["start_content_structure"] = 1
+            self.save_checkpoint()
+            self.save_content()
+        print("Completed ✓\n")
 
     def set_categories(self):
-        i = 1
-        for section, caracteristics in self.content['menu'].items():
+        print("\n-------CATEGORIES SET-------")
+        if not self.checkpoint["set_categories"]:
+            idx = 0
+            for section, characteristics in self.content['menu'].items():
 
-            if caracteristics['categorizable']:
-                categories_promt = f"""
-                Escribe como un Experto SEO
-                10 categorias relevantes para la seccion de {section} de una {self.store}.
-                En formato de lista de python. solo la lista de python.
-                siguiendo el siguiente formato: [categoria1, categoria2]
-                """
-                categories_response = get_completion(categories_promt)
-                categories_list = eval(categories_response)
-                caracteristics["categorias"] = []
-                for category in categories_list:
-                    caracteristics["categorias"].append({
-                        "name": category
-                    })
-            print(i, "category set")
-            i += 1
+                if characteristics['categorizable']:
+                    categories_promt = f"""
+                    Escribe como un Experto SEO
+                    10 categorias relevantes para la seccion de {section} de una {self.store}.
+                    En formato de lista de python. solo la lista de python.
+                    siguiendo el siguiente formato: [categoria1, categoria2]
+                    """
+                    categories_response = get_completion(categories_promt)
+                    categories_list = eval(categories_response)
+                    characteristics["categorias"] = []
+                    for category in categories_list:
+                        characteristics["categorias"].append({
+                            "name": category
+                        })
+                print(idx+1, "category set")
+                idx += 1
+            self.checkpoint["set_categories"] = 1
+            self.save_content()
+            self.save_checkpoint()
+
+        print("Completed ✓\n")
 
     def set_subcategories(self):
-        i = 1
-        for section, caracteristics in self.content['menu'].items():
-            if caracteristics['categorizable']:
-                for category in caracteristics["categorias"]:
+        print("\n-------SUBCATEGORIES SET-------\n")
+        section_idx = self.checkpoint["set_subcategories"]["section_idx"]
+        category_idx = self.checkpoint["set_subcategories"]["category_idx"]
+        idx = self.checkpoint["set_subcategories"]["idx"]
+        for section, characteristics in list(self.content['menu'].items())[section_idx:]:
+            if characteristics['categorizable']:
+
+                for category in characteristics["categorias"][category_idx:]:
                     subcategories_prompt = f"""
                     Escribe como un Experto SEO
                     10 Subcategorias relevantes para la categoria de "{category["name"]}" de la seccion de "{section}" de una {self.store}.
@@ -84,14 +115,31 @@ class StoreContentGenerator:
                         category["subcategorias"].append({
                             "name": subcategory
                         })
-                    print(i, "subcategory set")
-                    i += 1
+                    print(idx+1, "subcategory set")
+                    idx += 1
+                    category_idx += 1
+                    self.checkpoint["set_subcategories"]["category_idx"] = category_idx
+                    self.checkpoint["set_subcategories"]["idx"] = idx
+                    self.save_checkpoint()
+                    self.save_content()
+                category_idx = 0
+                self.checkpoint["set_subcategories"]["category_idx"] = category_idx
+                self.save_checkpoint()
+
+            section_idx += 1
+            self.checkpoint["set_subcategories"]["section_idx"] = section_idx
+            self.save_checkpoint()
+        print("Completed ✓\n")
 
     def set_products(self):
-        i = 1
+        print("\n-------PRODUCTS SET-------")
+        category_idx = self.checkpoint["set_products"]["category_idx"]
+        subcategory_idx = self.checkpoint["set_products"]["subcategory_idx"]
+        idx = self.checkpoint["set_products"]["idx"]
+
         if self.content['menu']['productos']:
-            for category in self.content['menu']['productos']['categorias']:
-                for subcategory in category['subcategorias']:
+            for category in self.content['menu']['productos']['categorias'][category_idx:]:
+                for subcategory in category['subcategorias'][subcategory_idx:]:
                     products_prompt = f"""
                     Escribe como un Experto en la Busqueda de {self.products} en Amazon.
                     20 productos relevantes para la subcategoria de "{subcategory["name"]}" de la categoria de "{category["name"]}" de  la seccion de "productos"  de una {self.store}. 
@@ -103,12 +151,44 @@ class StoreContentGenerator:
                     """
                     products_response = get_completion(products_prompt)
                     products_list = eval(products_response)
-                    subcategory["products"] = products_list
-                    print(i, "subcategory products set")
-                    i += 1
+                    subcategory["productos"] = products_list
+                    print(idx+1, "subcategory products set")
+                    idx += 1
+                    subcategory_idx += 1
+                    self.checkpoint["set_products"]["subcategory_idx"] = subcategory_idx
+                    self.checkpoint["set_products"]["idx"] = idx
+                    self.save_checkpoint()
+                    self.save_content()
+                subcategory_idx = 0
+                self.checkpoint["set_products"]["subcategory_idx"] = subcategory_idx
+                self.save_checkpoint()
+
+                category_idx += 1
+                self.checkpoint["set_products"]["category_idx"] = category_idx
+                self.save_checkpoint()
+
+        print("Completed ✓\n")
 
     def get_current_content(self):
         return self.content
+
+    def load_checkpoint(self):
+        try:
+            with open('checkpoint.json', 'r') as file:
+                self.checkpoint = json.load(file)
+        except FileNotFoundError:
+            self.save_checkpoint()
+
+    def save_checkpoint(self):
+        with open('checkpoint.json', 'w') as file:
+            json.dump(self.checkpoint, file)
+
+    def load_content(self):
+        try:
+            with open('content.json', 'r') as file:
+                self.content = json.load(file)
+        except FileNotFoundError:
+            self.save_content()
 
     def save_content(self):
         with open('content.json', 'w') as file:
--- main.py
+++ main.py
@@ -5,19 +5,17 @@ import json
 products = "Productos Ecologicos y Sostenibles"
 store = f"Tienda en linea de {products}"
 
+
 content_generator = StoreContentGenerator(store, products)
 
+content_generator.load_checkpoint()
+content_generator.load_content()
 content_generator.start_content_structure()
-print("\n--------INITIAL STRUCTURE SET-------\n")
 content_generator.set_categories()
-print("\n-------CATEGORIES SET-------\n")
 content_generator.set_subcategories()
-print("\n-------SUBCATEGORIES SET-------\n")
 content_generator.set_products()
-print("\n-------PRODUCTS SET-------\n")
 content_generator.save_content()
 print("\n-------CONTENT SAVED-------\n")
-
 print(content_generator.get_current_content())
 
 
